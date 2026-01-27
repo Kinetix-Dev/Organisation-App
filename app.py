52e6deded5253ae1ca0ebc870ab7125ec7ecf7ab
Debarshi Gayen <gayen.rishi12@gmail.com>
2026-01-27 12:37:36 +0000

    Created templates folder and login.html for User Auth page, added Bootstrap CSS to the project for styling

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def signup():
-    return 'This is the login page.'
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555)
