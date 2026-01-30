2af8b5ced8e77c5a410db5ce7b80a7b58e51d58e
Debarshi Gayen <gayen.rishi12@gmail.com>
2026-01-30 18:53:39 +0000

    Created init.db and schema.sql for database setup

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 import os
 import sys
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
-def signup():
+def redirect_signup():
+    return redirect('/register')
+
+@app.route('/register')
+def register():
     return render_template('login.html')
 
 if __name__ == '__main__':
---
+++ schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    total_points INTEGER DEFAULT 0
+);
+
+CREATE TABLE tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL
+    title TEXT NOT NULL
+    task_type TEXT,
+    difficulty TEXT,
+    points_value INTEGER,
+    is recurring BOOLEAN DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+)
